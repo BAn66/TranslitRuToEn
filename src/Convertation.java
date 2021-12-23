055c0c4266d455b865e1eb5490f6254853f7d8cd
BAn66 <thetanqel@gmail.com>
2021-12-23 16:12:56 +0400

    change constructor

--- src/Convertation.java
+++ src/Convertation.java
@@ -4,45 +4,12 @@ import java.util.*;
 
 public class Convertation {
     private String pathStr;
-    private HashMap<String, String> alphabit;
+    private final HashMap<String, String> alphabit;
 
 
     public Convertation(String pathStr) {
         this.pathStr = pathStr;
-        this.alphabit = new HashMap<String, String>();
-        alphabit.put("а", "a");
-        alphabit.put("б", "b");
-        alphabit.put("в", "v");
-        alphabit.put("г", "g");
-        alphabit.put("д", "d");
-        alphabit.put("е", "e");
-        alphabit.put("ё", "yo");
-        alphabit.put("ж", "zh");
-        alphabit.put("з", "z");
-        alphabit.put("и", "i");
-        alphabit.put("й", "y");
-        alphabit.put("к", "k");
-        alphabit.put("л", "l");
-        alphabit.put("м", "m");
-        alphabit.put("н", "n");
-        alphabit.put("о", "o");
-        alphabit.put("п", "p");
-        alphabit.put("р", "r");
-        alphabit.put("с", "s");
-        alphabit.put("т", "t");
-        alphabit.put("у", "u");
-        alphabit.put("ф", "f");
-        alphabit.put("х", "kh");
-        alphabit.put("ц", "ts");
-        alphabit.put("ч", "ch");
-        alphabit.put("ш", "sh");
-        alphabit.put("щ", "shch");
-        alphabit.put("ъ", "");
-        alphabit.put("ы", "y");
-        alphabit.put("ь", "");
-        alphabit.put("э", "e");
-        alphabit.put("ю", "yu");
-        alphabit.put("я", "ya");
+        this.alphabit = new AlphabitRuEnTranslit().getAlphabit();
     }
 
     public String getPath() {
@@ -71,6 +38,7 @@ public class Convertation {
                         boolean isUpper = Character.isUpperCase(symbolsNameFile[i]);//Проверка на заглавную букву
                         String OldSymbolInNameFile = String.valueOf(symbolsNameFile[i]);
                         String NewSymbolInNameFile = "";
+
                         for (Map.Entry<String, String> pair : alphabit.entrySet()) {
                             String key = pair.getKey();                      //ключ
                             String value = pair.getValue();                  //значение
